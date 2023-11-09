fa351a09d28b4966f3fe672f73cbd7e2e241fc20
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-11-09 13:41:13 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'mobx-react';
+import App from './App';
 import Utils from './utils/utils';
-import abpUserConfigurationService from './services/abpUserConfigurationService';
+// import abpUserConfigurationService from './services/abpUserConfigurationService';
 import initializeStores from './stores/storeInitializer';
 import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.less';
 import './styles/shared.css';
 
-declare var abp: any;
+// declare var abp: any;
 
 Utils.setLocalization();
 
-
-
-abpUserConfigurationService.getAll().then(data => {
-  Utils.extend(true, abp, data.data.result);
-  abp.clock.provider = Utils.getCurrentClockProvider(data.data.result.clock.provider);
+// abpUserConfigurationService.getAll().then(data => {
+//   Utils.extend(true, abp, data.data.result);
+//   abp.clock.provider = Utils.getCurrentClockProvider(data.data.result.clock.provider);
 
   // if (abp.clock.provider.supportsMultipleTimezone) {
   //   moment.tz.setDefault(abp.timing.timeZoneInfo.iana.timeZoneId);
@@ -36,4 +34,4 @@ abpUserConfigurationService.getAll().then(data => {
   );
 
   registerServiceWorker();
-});
+// });
